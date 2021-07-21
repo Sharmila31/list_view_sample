a9232724ee963f2958af331888d45b13cb5e45b8
stssharmilak <sharmila.k@dotnetethics.com>
2021-07-21 21:50:40 +0530

    Design changes done

---
+++ lib/MyThemeData.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyThemeData
+{
+  static const PrimaryColor =  Color(0xFFf26522);
+  static const StatusBarColor =  Color(0xFFff7737);
+  static const PrimaryDarkColor =  Color(0xFFff5450);
+  static const ErrorColor =  Color(0xFFE04F5F);
+  static const FieldTextColor =  Color(0xFF333333);
+  static const LabelTextColor =  Color(0xFFA9A9A9);
+  static const GreyBackgroundColor =  Color(0xFFEAEAEA);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'Dispatch.dart';
+import 'MyThemeData.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MaterialApp(
     home: BaseWidget(),
+    theme: ThemeData(
+      primaryColor: MyThemeData.PrimaryColor,
+    ),
   ));
 }
 
@@ -29,84 +34,124 @@ class BaseWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    setSystemBarTheme();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('ListView Sample'),
+        brightness: Brightness.dark,
       ),
       body: ListView.builder(
         itemCount: dispatchList.length,
         itemBuilder: (context, index) {
           return Container(
-            margin: EdgeInsets.all(15),
+            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
             child: Card(
-              elevation: 4,
+              elevation: 6,
               child: Padding(
                 padding: EdgeInsets.all(15),
-                child: Column(
+                child: Row(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            dispatchList[index].DispatchNumber,
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  dispatchList[index].DispatchNumber,
+                                  style: TextStyle(
+                                    color: MyThemeData.PrimaryColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              if (dispatchList[index].Status != null)
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: MyThemeData.GreyBackgroundColor,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(20.0),
+                                    ),
+                                    border: Border.all(
+                                      color: MyThemeData.GreyBackgroundColor,
+                                    ),
+                                  ),
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(horizontal: 8),
+                                    padding: const EdgeInsets.all(5.0),
+                                    child: Text(dispatchList[index].Status!),
+                                    //nullable check as Status is declared as final
+                                  ),
+                                ),
+                            ],
                           ),
-                        ),
-                        if (dispatchList[index].Status != null)
-                          Text(dispatchList[index].Status!),
-                        //nullable check as Status is declared as final
-                      ],
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 25,
-                          height: 25,
-                          child: Image.asset('assets/images/pick_up_img.png'),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Expanded(
-                          child: Text(dispatchList[index].PickUpLocation),
-                        ),
-                        Text(
-                          dispatchList[index].PickUpDate,
-                          style: italicFontStyle,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          width: 25,
-                          height: 25,
-                          child: Image.asset('assets/images/delivery_img.png'),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Expanded(
-                          child: Text(
-                            dispatchList[index].DeliveryLocation,
+                          SizedBox(
+                            height: 2,
                           ),
-                        ),
-                        if (dispatchList[index].DeliveryDate != null)
-                          Container(
-                            child: Text(
-                              dispatchList[index].DeliveryDate!,
-                              textAlign: TextAlign.end,
-                              style: italicFontStyle,
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 25,
+                                height: 25,
+                                child: Image.asset(
+                                    'assets/images/pick_up_img.png'),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Expanded(
+                                child: Text(dispatchList[index].PickUpLocation),
+                              ),
+                              Text(
+                                dispatchList[index].PickUpDate,
+                                style: italicFontStyle,
+                              ),
+                            ],
                           ),
-                      ],
+                          SizedBox(
+                            height: 2,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                width: 25,
+                                height: 25,
+                                child: Image.asset(
+                                    'assets/images/delivery_img.png'),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  dispatchList[index].DeliveryLocation,
+                                ),
+                              ),
+                              if (dispatchList[index].DeliveryDate != null)
+                                Container(
+                                  child: Text(
+                                    dispatchList[index].DeliveryDate!,
+                                    textAlign: TextAlign.end,
+                                    style: italicFontStyle,
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                        width: 5,
+                        height: 5,
+                        alignment: Alignment.centerRight,
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          color: MyThemeData.PrimaryColor,
+                          size: 15,
+                        ),
                     ),
                   ],
                 ),
@@ -117,4 +162,12 @@ class BaseWidget extends StatelessWidget {
       ),
     );
   }
+}
+
+void setSystemBarTheme() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: MyThemeData.StatusBarColor,
+    // navigation bar color
+    statusBarColor: MyThemeData.StatusBarColor, // status bar color
+  ));
 }
